79783cf74083877d54632e18f83642348ba91276
Zhuliucao <aether1989@gmail.com>
2014-11-24 14:19:47 +0800

    服务端后台编码问题修复
    解码拦截器问题

--- code/server/SmartHome/src/cn/fuego/smart/home/webservice/up/rest/interceptor/DecodeInterceptor.java
+++ code/server/SmartHome/src/cn/fuego/smart/home/webservice/up/rest/interceptor/DecodeInterceptor.java
@@ -1,14 +1,15 @@
 /**   
-* @Title: EncodeInterceptor.java 
-* @Package cn.fuego.smart.home.webservice.from.client.rest.interceptor 
-* @Description: TODO
-* @author Tang Jun   
-* @date 2014-10-29 下午11:50:19 
-* @version V1.0   
-*/ 
+ * @Title: EncodeInterceptor.java 
+ * @Package cn.fuego.smart.home.webservice.from.client.rest.interceptor 
+ * @Description: TODO
+ * @author Tang Jun   
+ * @date 2014-10-29 下午11:50:19 
+ * @version V1.0   
+ */
 package cn.fuego.smart.home.webservice.up.rest.interceptor;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import javax.ws.rs.WebApplicationException;
@@ -21,48 +22,50 @@ import org.jboss.resteasy.spi.interception.MessageBodyReaderInterceptor;
 
 import cn.fuego.common.log.FuegoLog;
 
- /** 
- * @ClassName: EncodeInterceptor 
+/**
+ * @ClassName: EncodeInterceptor
  * @Description: TODO
  * @author Tang Jun
- * @date 2014-10-29 下午11:50:19 
- *  
+ * @date 2014-10-29 下午11:50:19
+ * 
  */
 @Provider
 @ServerInterceptor
 @Precedence("ENCODER")
-public class DecodeInterceptor  implements MessageBodyReaderInterceptor
+public class DecodeInterceptor implements MessageBodyReaderInterceptor
 {
 	private FuegoLog log = FuegoLog.getLog(DecodeInterceptor.class);
 
-	/* (non-Javadoc)
-	 * @see org.jboss.resteasy.spi.interception.MessageBodyReaderInterceptor#read(org.jboss.resteasy.spi.interception.MessageBodyReaderContext)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.jboss.resteasy.spi.interception.MessageBodyReaderInterceptor#read
+	 * (org.jboss.resteasy.spi.interception.MessageBodyReaderContext)
 	 */
 	@Override
 	public Object read(MessageBodyReaderContext arg0) throws IOException,
 			WebApplicationException
 	{
 
-		 int i =0;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		int i = -1;
+		while ((i = arg0.getInputStream().read()) != -1)
+		{
+			baos.write(i);
+		}
+		String req = new String(baos.toByteArray(),"UTF-8");
 
-		 StringBuffer body = new StringBuffer();
-		 while((i = arg0.getInputStream().read())!= -1)
-		 {
-			 body.append((char)i);
-		 }
-		 if(null != arg0.getInputStream())
-		 {
-			 arg0.getInputStream().close();
-		 }
-		 log.info("the request message is "+body);
-		 
- 
-		 ByteArrayInputStream bais=new ByteArrayInputStream(body.toString().getBytes());
-		 arg0.setInputStream(bais);
-		 
+		if (null != arg0.getInputStream())
+		{
+			arg0.getInputStream().close();
+		}
+		log.info("the request message is " + req);
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+		arg0.setInputStream(bais);
+
 		return arg0.proceed();
 	}
-
- 
 
 }
